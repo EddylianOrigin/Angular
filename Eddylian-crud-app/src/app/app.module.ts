abdb70ba8865d5a9aa4f93701505552de0c6e3d3
Sylvain Eddy <nguesson@rhrk.uni-kl.de>
2023-04-10 23:34:05 +0200

    App Updated by added some features

--- Eddylian-crud-app/src/app/app.module.ts
+++ Eddylian-crud-app/src/app/app.module.ts
@@ -13,6 +13,9 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from "@angular/material/input";
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatNativeDateModule} from "@angular/material/core";
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -31,6 +34,9 @@ import {MatNativeDateModule} from "@angular/material/core";
     MatInputModule,
     MatDatepickerModule,
     MatNativeDateModule,
+    MatRadioModule,
+    MatSelectModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Eddylian-crud-app/src/app/employee-add-edit/employee-add-edit.component.ts
+++ Eddylian-crud-app/src/app/employee-add-edit/employee-add-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-employee-add-edit',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./employee-add-edit.component.scss']
 })
 export class EmployeeAddEditComponent {
+  empForm: FormGroup;
 
+  education: string[] = [
+    'Matric',
+    'Diploma',
+    'Intermediate',
+    'Graduate',
+    'Post Graduate',
+  ]
+
+  constructor(private _fb: FormBuilder) {
+    this.empForm = this._fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      dob: '',
+      gender: '',
+      education: '',
+      company: '',
+      experience: '',
+      package: '',
+    })
+  }
 }
